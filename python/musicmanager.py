d99f407afd1eacd12672c31acc1d1b32370d88a5
Алексей Максимов <maksimov@raa-st.ru>
2013-11-05 16:35:14 +0400

    Создание списка треков

--- python/musicmanager.py
+++ python/musicmanager.py
@@ -10,12 +10,12 @@ import sys
 from argparse import ArgumentParser
 
 
-
 class CopyMusic :
 
-    opt = None
     def __init__(self):
+        opt = None
         self.bad_char_re = re.compile(r'[<>:"\|?*]+')
+        self.check_format_re = re.compile(r".*/.+/\d{4} .+/(\d{2,3} - \S*\.mp3$)")
 
     def __clean_path(self, path):
         return self.bad_char_re.sub('_', path)
@@ -60,7 +60,29 @@ class CopyMusic :
             eyeD3 = imp.load_source('eyed3', '/usr/bin/eyeD3')
             eyeD3.main()
 
-        pass
+    def write_tags(self):
+        for path in self.opt.src:
+            path = os.path.abspath(path)
+            if not os.path.exists(path):
+                print ("Path %s not exists. Ignore" % path)
+
+            tracks = []
+            error_tracks = []
+            # формируем список треков
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    track_path = "%s/%s" % (root, file)
+                    print (track_path)
+                    if not self.check_format_re.match(track_path):
+                        error_tracks.append(track_path)
+                    else:
+                        tracks.append(track_path)
+
+                if not error_tracks:
+                    print "Format error in tracks:"
+                    for track_path in error_tracks:
+                        print track_path
+
 
     def main(self):
         parser = ArgumentParser(description="Options")
@@ -69,6 +91,7 @@ class CopyMusic :
         parser.add_argument("-a", dest='src', action='append', help='Sources dirs')
         parser.add_argument("-d", dest='dst', action='store', help='Destination dir', required=True)
         parser.add_argument("-b", dest='base', action='store', help='Base music dir', required=True)
+        parser.add_argument('-')
 
         self.opt = parser.parse_args()
 
