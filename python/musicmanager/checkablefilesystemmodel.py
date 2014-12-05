f3dc0ec9c9e9b9a5345b52c115fdf280c9cb265c
Алексей Максимов <destrd@gmail.com>
2014-12-05 21:45:12 +0300

    Просмотр коллекции

---
+++ python/musicmanager/checkablefilesystemmodel.py
@@ -0,0 +1,87 @@
+__author__ = 'destr'
+
+from PyQt5.QtWidgets import QFileSystemModel
+from PyQt5 import QtCore
+
+
+class State:
+    def __init__(self):
+        self.checked = False
+        self.path = None
+
+class CheckableFileSystemModel(QFileSystemModel):
+    def __init__(self, parent=None):
+        QFileSystemModel.__init__(self, parent)
+        self.states = dict()
+
+    def data(self, index, role=None):
+        if role == QtCore.Qt.CheckStateRole and index.column() == 0:
+            filepath = self.filePath(index)
+            if filepath in self.states:
+                return self.states[filepath]
+            else:
+                return QtCore.Qt.Unchecked
+
+        return QFileSystemModel.data(self, index, role)
+
+    def setData(self, index, data, role=None):
+        if role == QtCore.Qt.CheckStateRole:
+            self.__setState(index, data)
+            self.__childState(index, data)
+
+            p = index.parent()
+            while self.__partialCheck(p):
+                p = p.parent()
+            return True
+
+        return QFileSystemModel.setData(index, data, role)
+
+    def flags(self, index):
+        return QFileSystemModel.flags(self, index) | QtCore.Qt.ItemIsUserCheckable
+
+    def __partialCheck(self, index):
+        statekey = self.filePath(index)
+        if self.rootPath() == statekey:
+            return None
+        state = None
+
+
+        for row in range(0, self.rowCount(index)):
+            childIndex = self.index(row, 0, index)
+            statekey = self.filePath(childIndex)
+
+            if row == 0:
+                state = self.__state(statekey)
+
+            if state != self.__state(statekey) :
+                state = QtCore.Qt.PartiallyChecked
+                break
+
+        self.__setState(index, state)
+        self.dataChanged.emit(index, index)
+        return state
+
+    def __childState(self, index, data):
+        while self.canFetchMore(index):
+            self.fetchMore(index)
+
+        for row in range(0, self.rowCount(index)):
+            childIndex = self.index(row, 0, index)
+            self.__setState(childIndex, data)
+            self.dataChanged.emit(childIndex, childIndex)
+
+            self.__childState(childIndex, data)
+
+
+    def __state(self, key):
+        if key in self.states:
+            return QtCore.Qt.Checked
+
+        return QtCore.Qt.Unchecked
+
+    def __setState(self, index, data):
+        filename = self.filePath(index)
+        if data == QtCore.Qt.Unchecked:
+            self.states.pop(filename, None)
+        else:
+            self.states[filename] = data
--- python/musicmanager/musicmanagergui.py
+++ python/musicmanager/musicmanagergui.py
@@ -1,11 +1,14 @@
 __author__ = 'destr'
 
 import sys
+import os
 
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, QByteArray
 from PyQt5.QtCore import QSettings
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
 from ui_musicmanager import Ui_MusicManager
+
+from checkablefilesystemmodel import CheckableFileSystemModel
 
 class MusicManager(QDialog):
     def __init__(self, parent=None):
@@ -13,13 +16,44 @@ class MusicManager(QDialog):
         self.ui = Ui_MusicManager()
         self.ui.setupUi(self)
 
+        self.model = CheckableFileSystemModel(self.ui.treeView)
+        self.ui.treeView.setModel(self.model)
+
         self.ui.pushButtonCancel.clicked.connect(self.cancel)
+        self.ui.pushButtonSelectMusicDir.clicked.connect(self.selectMusicDir)
+        self.ui.lineEditMusicDir.textChanged.connect(self.setDir)
+
+        self.__loadSettings()
+
+    def setDir(self, value):
+        self.model.setRootPath(value)
+        self.ui.treeView.setRootIndex(self.model.index(value))
 
     def closeEvent(self, event):
-        print("CloseEvent")
+        self.__saveSettings()
 
     def cancel(self):
         print("Cancel")
+
+    def selectMusicDir(self):
+        path = QFileDialog.getExistingDirectory(self, "Select music dir", os.path.expanduser('~'))
+        if path:
+            self.ui.lineEditMusicDir.setText(path)
+
+    def __saveSettings(self):
+        sets = QSettings()
+        sets.beginGroup("MusicManager")
+        sets.setValue("geometry", self.saveGeometry())
+        sets.setValue("musicdir", self.ui.lineEditMusicDir.text())
+        sets.endGroup()
+
+    def __loadSettings(self):
+        sets = QSettings()
+        sets.beginGroup("MusicManager")
+        self.ui.lineEditMusicDir.setText(sets.value("musicdir"))
+        self.restoreGeometry(sets.value("geometry", QByteArray()))
+        sets.endGroup()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
