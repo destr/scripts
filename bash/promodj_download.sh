88a539bd5a8654b2c53a6302a233c87dd325a36b
Алексей Максимов <destrd@gmail.com>
2019-09-04 22:58:47 +0300

    range from cmd

--- bash/promodj_download.sh
+++ bash/promodj_download.sh
@@ -4,9 +4,15 @@ base_url="https://promodj.com/djyura/groups/17664/efiri_programmi_intelligent_So
 
 #grep tool__downloads | grep -o 'href=\"[^\"]\+"'
 
+begin=$1
+end=$2
+if [[ -z $end ]];then
+    echo "Empty range"
+    exit 1
+fi
 
-for p in $(seq 1 2);do
+for p in $(seq $begin $end);do
     url=$(printf $base_url $p)
-    wget -q $url -O - | grep tool__downloads | grep -o 'href=\"[^\"]\+"' | cut -d= -f2 | tr -d \"
+    wget -q $url -O - | grep tool__downloads | grep -o 'href=\"[^\"]\+"' | cut -d= -f2 | tr -d \" | sort
 done
 
